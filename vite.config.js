ff445f08047ff251c0bd5f6233107edbacf1fe69
Frankpesta <franklinolisaemeka3@gmail.com>
2024-12-23 07:44:10 +0100

    feat:successfully showing balances

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		nodePolyfills({
+			protocolImports: true,
+		}),
+	],
+	define: {
+		"process.env": {},
+		"process.argv": "[]",
+		"process.browser": "true", // Shim `process.browser` as true for browser compatibility
+		"process.version": '"0.0.0"', // Provide a dummy value for `process.version`
+	},
+});
